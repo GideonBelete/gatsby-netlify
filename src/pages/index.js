0368c6e5415fa6f0e2a8e6cd1fbab88bf5a5abca
Gideon Belete <gbelete@gmail.com>
2020-08-20 14:56:29 -0600

    update to setup message

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,12 +2,7 @@ import React from "react"
 
 const IndexPage = () => (
   <div>
-    <h2>Gatsby + Netlify + Git CKit v1.0</h2>
-    <ol>
-      <li>Clone repo via <em>git clone https://(url)</em></li>
-      <li>Run command <em>gatsby develop</em> to start https://localhost:8000/ <em>git clone https://(url)</em></li>
-      <li>Use <em>git commit -m "comment"</em> to commit local changes to repo</li>
-    </ol>
+
   </div>
 )
 
